9e0220285ad4a935ca9defdb7f09d6218e715cd2
Klissan <Klissan@Klissan>
2024-05-15 16:41:06 -0700

    mod.malakai: add files

---
+++ mods/malakai/input/script/campaign/mod/malakai_adventures.klissan.lua
@@ -0,0 +1,32 @@
+
+
+
+core:add_listener(
+	Klissan_CH:get_listener_name('malakai_adventure_activated_grant_units'),
+	"RitualCompletedEvent",
+	function (context)
+        return context:ritual():ritual_category() == 'MALAKAI_ADVENTURE_ACTIVATE'
+    end,
+	function(context)
+		local general_support_army_lookup_str = cm:char_lookup_str(cm:get_military_force_by_cqi(MGSWT.malakai_support_army_cqi):general_character())
+		console_print(''..MGSWT.malakai_support_army_cqi)
+		local ritual_key = context:ritual():ritual_key()
+		local result_str = MGSWT.croot:Call(string.format([=[
+			(
+				key = '%s',
+				payload = DatabaseRecordContext('CcoRitualRecord', key).CompletionPayloadContext.CampaignPayloadContext,
+				res = payload.UnitComponents.JoinString(UnitContext.Key + ',' + Amount, ';')
+			) => res
+		]=], ritual_key))
+    	local unit_components = string.split(result_str, ';')
+		for i=1, #unit_components do
+			local unit = string.split(unit_components[i], ',')
+			local unit_key, unit_amount = unit[1], tonumber(unit[2])
+			for _=1,unit_amount do
+				cm:grant_unit_to_character(general_support_army_lookup_str, unit_key)
+			end
+		end
+	end,
+	true
+)
+
