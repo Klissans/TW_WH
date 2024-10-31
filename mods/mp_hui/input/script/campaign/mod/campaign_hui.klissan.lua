c2f901c1d2fb5a036b71cb1cf02c3d20724bebd0
Klissan <Klissan@Klissan>
2024-10-30 20:23:34 -0700

    mod.hui: screen offset in campaign

--- mods/mp_hui/input/script/campaign/mod/campaign_hui.klissan.lua
+++ mods/mp_hui/input/script/campaign/mod/campaign_hui.klissan.lua
@@ -1,13 +1,12 @@
 local function move_info_panel()
     local info_panel = find_uicomponent(core:get_ui_root(), 'hud_campaign', 'unit_info_panel_adopter', 'unit_information_parent', 'unit_info_panel_holder')
-    local x, y = info_panel:GetDockOffset()
-    if y < 1000 then
-        info_panel:SetDockOffset(10, -270)
+    local x, y = core:get_screen_resolution()
+    if y > 1000 then
+        info_panel:SetDockOffset(10, - math.floor(y / 10))
     else
-        info_panel:SetDockOffset(10, -385)
+        info_panel:SetDockOffset(10, 0)
     end
 end
-
 
 cm:add_ui_created_callback(function()
     cm:add_post_first_tick_callback(
