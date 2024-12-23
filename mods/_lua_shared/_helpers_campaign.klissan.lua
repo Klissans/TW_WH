ce3d23b03624e109637eb21ba2b0bc62da0b1a5e
Klissan <Klissan@Klissan>
2024-12-23 09:14:56 -0800

    mod.helpers: effects

--- mods/_lua_shared/_helpers_campaign.klissan.lua
+++ mods/_lua_shared/_helpers_campaign.klissan.lua
@@ -30,3 +30,40 @@ function Klissan_CH:croot()
     end
     return self.croot_cco
 end
+
+
+function Klissan_CH:parse_effects_string(effects_str)
+    local effects_table = {}
+    local effect_value_pairs = string.split(effects_str, ';')
+    for i=1, #effect_value_pairs do
+        if effect_value_pairs[i]:len() ~= 0 then
+            local effect_value = string.split(effect_value_pairs[i], ',')
+            local effect_key, value = effect_value[1], math.floor(tonumber(effect_value[2]))
+            if effects_table[effect_key] ~= nil then
+                effects_table[effect_key] = effects_table[effect_key] + value
+            else
+                effects_table[effect_key] = value
+            end
+        end
+    end
+    local force_movement_key = 'wh_main_effect_force_all_campaign_movement_range'
+    local agent_movement_key = 'wh_main_effect_agent_movement_range_mod'
+    if effects_table[agent_movement_key] ~= nil then
+        if effects_table[force_movement_key] == nil then
+            effects_table[force_movement_key] = 0
+        end
+        effects_table[force_movement_key] = effects_table[force_movement_key] + effects_table[agent_movement_key]
+        effects_table[agent_movement_key] = nil
+    end
+    return effects_table
+end
+
+
+function Klissan_CH:get_army_effects(source_army_cqi)
+    local skills_str = Klissan_CH:croot():Call(string.format([=[
+        (
+            army = CampaignRoot.MilitaryForceList.FirstContext(CQI == %d)
+        ) => army.GetAllEffectsAsString()
+    ]=], source_army_cqi))
+    return Klissan_CH:parse_effects_string(skills_str)
+end
